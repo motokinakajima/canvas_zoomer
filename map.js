1e8f9485c993e8cac9f58267d9cc13f5f821cc92
Motoki Nakajima <motokinakajima0912@gmail.com>
2024-10-23 17:05:37 -0400

    cannot scroll

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 const map = document.getElementById('map_layer');
-const ctx = map.getContext('2d');
+const mapCtx = map.getContext('2d');
 
 let characterX = 50;
 let characterY = 50;
@@ -10,9 +10,7 @@ const mapImage = new Image();
 mapImage.src = 'map.png';
 
 mapImage.onload = () => {
-    ctx.drawImage(mapImage, 0, 0, map.width, map.height);
-    ctx.fillStyle = 'red';
-    ctx.fillRect(characterX, characterY, 10, 10);
+    drawMap();
 }
 
 function resizeCanvas() {
@@ -23,13 +21,13 @@ function resizeCanvas() {
 
 function updateMapPosition(x, y) {
     characterX = x;
-    characterY = y;d
+    characterY = y;
     drawMap();
 }
 
 function drawMap() {
-    ctx.clearRect(0, 0, map.width, map.height);
-    ctx.drawImage(
+    mapCtx.clearRect(0, 0, map.width, map.height);
+    mapCtx.drawImage(
         mapImage,
         characterX - (map.width / (2 * zoom)),
         characterY - (map.height / (2 * zoom)),
